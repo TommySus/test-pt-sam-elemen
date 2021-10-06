0d6a2d3009188dd3f632da48be79bc9eb65fcbd1
TommySus <tommysusanto77@gmail.com>
2021-10-06 14:59:09 +0700

    menambah update user 1. menambahkan Route untuk update User dengan Parameter ID

--- RestApi/controllers/userController.js
+++ RestApi/controllers/userController.js
@@ -2,15 +2,31 @@ const { User } = require('../models')
 
 class UserController {
     static register(req,res,next) {
+        let last_name = req.body.last_name
+        let address = req.body.address
+        let city = req.body.city
+        let province = req.body.province
+        if (!last_name || !last_name.trim()) {
+            last_name = null
+        }
+        if (!address || !address.trim()) {
+            address = null
+        }
+        if (!city || !city.trim()) {
+            city = null
+        }
+        if (!province || !province.trim()) {
+            province = null
+        }
         const obj = {
             username: req.body.username, 
             email: req.body.email,
             password: req.body.password,
             first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            address: req.body.address,
-            city: req.body.city,
-            province: req.body.province,
+            last_name: last_name,
+            address: address,
+            city: city,
+            province: province,
             updatedAt: new Date(),
             createdAt: new Date
         }
@@ -26,6 +42,68 @@ class UserController {
                 city: data.city,
                 province: data.province,
             })
+        })
+        .catch(error => {
+            next(error)
+        })
+    }
+
+
+    static updateUser(req,res,next) {
+        let last_name = req.body.last_name
+        let address = req.body.address
+        let city = req.body.city
+        let province = req.body.province
+        if (!last_name || !last_name.trim()) {
+            last_name = null
+        }
+        if (!address || !address.trim()) {
+            address = null
+        }
+        if (!city || !city.trim()) {
+            city = null
+        }
+        if (!province || !province.trim()) {
+            province = null
+        }
+        const obj = {
+            username: req.body.username, 
+            email: req.body.email,
+            password: req.body.password,
+            first_name: req.body.first_name,
+            last_name: last_name,
+            address: address,
+            city: city,
+            province: province
+        }
+        User.update(obj,{where: {id: req.params.id}})
+        .then(data => {
+            if (data) {
+                return User.findOne({where: {id: req.params.id}})
+                .then(data2 => {
+                   res.status(200).json({
+                        id:data2.id,
+                        username: data2.username, 
+                        email: data2.email,
+                        first_name: data2.first_name,
+                        last_name: data2.last_name,
+                        address: data2.address,
+                        city: data2.city,
+                        province: data2.province,
+                   })
+                })
+                .catch(error => {
+                    throw {
+                        status: 404,
+                        message: 'User not found'
+                    }
+                }) 
+            } else {
+                throw {
+                    status: 404,
+                    message: 'User not found'
+                }
+            }
         })
         .catch(error => {
             next(error)
--- RestApi/routes/user.js
+++ RestApi/routes/user.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/userController')
 
 
 router.post("/",  UserController.register)
+router.put("/:id",  UserController.updateUser)
 
 
 module.exports = router
